80a3ca0b9d7068f371a8ff867fa27007078242cf
Dario Kruger Junior <krugerdario1@gmail.com>
2021-08-21 15:15:37 -0300

    Adicionando nova rota

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,16 @@
 
 const routes = [
+
   {
     path: '/',
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Login.vue') }
+    ]
+  },
+
+  {
+    path: '/auth',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') }
